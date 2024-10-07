419258031fa91ae578fa9e6e04928af315cb92cd
Milan Cruz <milanmoreira@gmail.com>
2024-10-06 21:59:16 -0500

    added seed data with Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require "faker"
+
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price,
+    stock_quantity: Faker::Number.between(from: 1, to: 100),
+  )
+end
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
